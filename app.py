455046cb07cad67cba67a7ed2363645b336afe7b
ranir007 <rr27082021@gmail.com>
2023-01-20 13:24:59 +0530

    commit

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ model = pickle.load(open('trained_model.pkl', 'rb'))
 cv = pickle.load(open('transform.pkl', 'rb'))
 @app.route('/', methods=['GET'])
 def Home():
-    return render_template('index.html')
+    return render_template('/index.html')
 
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
@@ -32,7 +32,7 @@ def predict():
         return str("The Category is: {}".format((prediction)))
         
     #else:
-        #return render_template('index.html')
+        #return render_template('Templates/index.html')
 
 if __name__=="__main__":
     app.run(debug=True)
