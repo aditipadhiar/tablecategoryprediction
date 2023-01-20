153ae751a6a3d5de5799f5794774f88383457fee
ranir007 <rr27082021@gmail.com>
2023-01-20 16:18:39 +0530

    commit changes

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ model = pickle.load(open('trained_model.pkl', 'rb'))
 cv = pickle.load(open('transform.pkl', 'rb'))
 @app.route('/', methods=['GET'])
 def Home():
-    return render_template('/index.html')
+    #return render_template('index.html')
+    return "Hello world!"
 
 standard_to = StandardScaler()
 @app.route("/predict", methods=['POST'])
@@ -29,10 +30,13 @@ def predict():
         vect = cv.transform(data).toarray()
         prediction=model.predict(vect)
         #return str(prediction)
-        return render_template('/index.html', prediction_text="The Category is: {}".format((prediction)))
-        
+        return render_template('index.html', prediction_text="The Category is: {}".format((prediction)))
+        #return str(prediction_text="The Category is: {}".format((prediction)))
+
     #else:
         #return render_template('index.html')
 
 if __name__=="__main__":
+    #response_API = requests.get('https://web-production-20ea.up.railway.app')
+    #print(response_API.text)
     app.run(debug=True)
